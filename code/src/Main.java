1ec223dd0989252be7ffccaf672f8108f2392ec0
Stefano Casagrande <stefano98casagrande@gmail.com>
2023-10-17 14:38:28 +0200

    update main

--- code/src/Main.java
+++ code/src/Main.java
@@ -9,46 +9,48 @@ import static bubble_sort.BubbleSort.bubbleSort;
 
 public class Main {
     public static void main(String[] args) {
-        int[] result = new int[0];
-        long randomArrayTime = Timer.measureExecutionTime(() -> Generator.randomArray(100, 0, 10000));
-        long randomArrayAscendingTime = Timer.measureExecutionTime(() -> Generator.randomArrayAscending(100, 0, 10000));
-        long randomArrayDescendingTime = Timer.measureExecutionTime(() -> Generator.randomArrayDescending(100, 0, 10000));
-
-        int[] insert_array = InsertionSort.returnSort(Generator.randomArray(100, 0, 10000));
-        int[] buble_array = (Generator.randomArray(100, 0, 10000));
-        bubbleSort(buble_array);
-
-        System.out.println("------TABELLA DEI TEMPI DI INSERT SORT -------------");
-        System.out.println(Arrays.toString(insert_array));
-        int[] random_array_insert = Generator.randomArray(100, 0, 10000);
-        int[] random_ascending_array_insert = Generator.randomArrayAscending(100, 0, 10000);
-        int[] random_descending_array_insert = Generator.randomArrayDescending(100, 0, 10000);
-
-         randomArrayTime = Timer.measureExecutionTime(() -> InsertionSort.returnSort(random_array_insert));
-         randomArrayAscendingTime = Timer.measureExecutionTime(() ->InsertionSort.returnSort(random_ascending_array_insert)) ;
-         randomArrayDescendingTime = Timer.measureExecutionTime(() ->InsertionSort.returnSort(random_descending_array_insert)) ;
-
-        System.out.println("Execution Time of Insertion sort on randomArray(): " + randomArrayTime + " ns.");
-        System.out.println("Execution Time of Insertion sort on randomArrayAscending(): " + randomArrayAscendingTime + " ns.");
-        System.out.println("Execution Time of Insertion sort on randomArrayDescending(): " + randomArrayDescendingTime + " ns.");
-
-        System.out.println("------TABELLA DEI TEMPI BUBBLE SORT -------------");
-
-
-        int[] random_array_bubble = Generator.randomArray(100, 0, 10000);
-        int[] random_ascending_array_bubble = Generator.randomArrayAscending(100, 0, 10000);
-        int[] random_descending_array_bubble = Generator.randomArrayDescending(100, 0, 10000);
-
+        long randomArrayTime, randomArrayAscendingTime, randomArrayDescendingTime;
+        int length = 10;
+        int low = 0;
+        int up = 100000;
+        System.out.println("#########################################################################################");
+        System.out.println("########################## TABELLA DEI TEMPI DI INSERTION SORT ##########################");
+        System.out.println("#########################################################################################");
+        int[] random_array_insert = Generator.randomArray(length, low, up);
+        int[] random_ascending_array_insert = Generator.randomArrayAscending(length, low, up);
+        int[] random_descending_array_insert = Generator.randomArrayDescending(length, low, up);
+        System.out.println("Il randomArray():" + Arrays.toString(random_array_insert));
+        System.out.println("Il randomArrayAscending(): " + Arrays.toString(random_ascending_array_insert));
+        System.out.println("Il randomArrayAscending(): " + Arrays.toString(random_descending_array_insert));
+        randomArrayTime = Timer.measureExecutionTime(() -> InsertionSort.returnSort(random_array_insert));
+        randomArrayAscendingTime = Timer.measureExecutionTime(() -> InsertionSort.returnSort(random_ascending_array_insert));
+        randomArrayDescendingTime = Timer.measureExecutionTime(() -> InsertionSort.returnSort(random_descending_array_insert));
+        System.out.println("Il randomArray() ordinato:" + Arrays.toString(random_array_insert));
+        System.out.println("Il randomArrayAscending() ordinato:" + Arrays.toString(random_ascending_array_insert));
+        System.out.println("Il randomArrayDescending() ordinato:" + Arrays.toString(random_descending_array_insert));
+        System.out.println("Execution Time di Insertion sort su randomArray(): " + randomArrayTime + " ns.");
+        System.out.println("Execution Time di Insertion sort su randomArrayAscending(): " + randomArrayAscendingTime + " ns.");
+        System.out.println("Execution Time di Insertion sort su randomArrayDescending(): " + randomArrayDescendingTime + " ns.");
+        System.out.println("#########################################################################################");
+        System.out.println("########################## TABELLA DEI TEMPI DI BUBBLE SORT #############################");
+        System.out.println("#########################################################################################");
+        int[] random_array_bubble = Generator.randomArray(length, low, up);
+        int[] random_ascending_array_bubble = Generator.randomArrayAscending(length, low, up);
+        int[] random_descending_array_bubble = Generator.randomArrayDescending(length, low, up);
+        System.out.println("Il randomArray():" + Arrays.toString(random_array_bubble));
+        System.out.println("Il randomArrayAscending(): " + Arrays.toString(random_ascending_array_bubble));
+        System.out.println("Il randomArrayAscending(): " + Arrays.toString(random_descending_array_bubble));
         randomArrayTime = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_array_bubble));
-        randomArrayAscendingTime = Timer.measureExecutionTime(() ->BubbleSort.bubbleSort(random_ascending_array_bubble)) ;
-        randomArrayDescendingTime = Timer.measureExecutionTime(() ->BubbleSort.bubbleSort(random_descending_array_bubble));
-
+        randomArrayAscendingTime = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_ascending_array_bubble)) ;
+        randomArrayDescendingTime = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_descending_array_bubble));
+        System.out.println("Il randomArray() ordinato:" + Arrays.toString(random_array_bubble));
+        System.out.println("Il randomArrayAscending() ordinato:" + Arrays.toString(random_ascending_array_bubble));
+        System.out.println("Il randomArrayDescending() ordinato:" + Arrays.toString(random_descending_array_bubble));
         System.out.println("Execution Time of Bubble sort on randomArray(): " + randomArrayTime + " ns.");
         System.out.println("Execution Time of Bubble sort on randomArrayAscending(): " + randomArrayAscendingTime + " ns.");
         System.out.println("Execution Time of Bubble sort on randomArrayDescending(): " + randomArrayDescendingTime + " ns.");
-
-        System.out.println(Arrays.toString(buble_array));
-
-
+        System.out.println("#########################################################################################");
+        System.out.println("######################################## FINE ###########################################");
+        System.out.println("#########################################################################################");
     }
 }
