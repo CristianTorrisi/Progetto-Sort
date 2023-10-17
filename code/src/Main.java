dc69cc579bd5e8daf2a06ec7d36775f8a4fee8c1
Stefano Casagrande <stefano98casagrande@gmail.com>
2023-10-17 14:42:51 +0200

    update main

--- code/src/Main.java
+++ code/src/Main.java
@@ -13,6 +13,19 @@ public class Main {
         int length = 10;
         int low = 0;
         int up = 100000;
+
+        int[] random_array = Generator.randomArray(length, low, up);
+        int[] random_ascending_array = Generator.randomArrayAscending(length, low, up);
+        int[] random_descending_array = Generator.randomArrayDescending(length, low, up);
+
+        int[] random_array_1 = Arrays.copyOf(random_array, random_array.length);
+        int[] random_ascending_array_1 = Arrays.copyOf(random_ascending_array, random_array.length);
+        int[] random_descending_array_1 = Arrays.copyOf(random_descending_array, random_array.length);
+
+        int[] random_array_2 = Arrays.copyOf(random_array, random_array.length);
+        int[] random_ascending_array_2 = Arrays.copyOf(random_ascending_array, random_array.length);
+        int[] random_descending_array_2 = Arrays.copyOf(random_descending_array, random_array.length);
+
         System.out.println("#########################################################################################");
         System.out.println("########################## TABELLA DEI TEMPI DI INSERTION SORT ##########################");
         System.out.println("#########################################################################################");
