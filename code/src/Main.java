ede5c1abf9a8ba92a8f8a86baf060dd7d7851ecc
davide cruciata <davidecruciata@gmail.com>
2023-10-18 14:31:12 +0200

    Update Main

--- code/src/Main.java
+++ code/src/Main.java
@@ -1,58 +1,66 @@
-import insert_sort.InsertionSort;
 import bubble_sort.BubbleSort;
+import insert_sort.InsertionSort;
 import utils.Generator;
 import utils.Timer;
+
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("#########################################################################################");
-        System.out.println("######################################### 2TEAM #########################################");
-        System.out.println("#########################################################################################");
-        int length = 1000; // 10 // 1000 // 100000
-        int low = 0;
-        int up = 1000000;
-        int[] random_array = Generator.randomArray(length, low, up);
-        int[] random_ascending_array = Generator.randomArrayAscendingSimple(length, low, up); // Generator.randomArrayAscending(length, low, up);
-        int[] random_descending_array = Generator.randomArrayDescendingSimple(length, low, up);// Generator.randomArrayDescending(length, low, up);
-        int[] random_array_1 = Arrays.copyOf(random_array, random_array.length);
-        int[] random_array_2 = Arrays.copyOf(random_array, random_array.length);
-        int[] random_ascending_array_1 = Arrays.copyOf(random_ascending_array, random_array.length);
-        int[] random_ascending_array_2 = Arrays.copyOf(random_ascending_array, random_array.length);
-        int[] random_descending_array_1 = Arrays.copyOf(random_descending_array, random_array.length);
-        int[] random_descending_array_2 = Arrays.copyOf(random_descending_array, random_array.length);
-        System.out.println("Il randomArray(): " + Arrays.toString(random_array));
-        System.out.println("Il randomArray(): " + Arrays.toString(random_ascending_array));
-        System.out.println("Il randomArrayAscending(): " + Arrays.toString(random_descending_array));
-        System.out.println("#########################################################################################");
-        System.out.println("########################## TABELLA DEI TEMPI DI INSERTION SORT ##########################");
-        System.out.println("#########################################################################################");
-        long randomArrayTime1, randomArrayAscendingTime1, randomArrayDescendingTime1;
-        randomArrayTime1 = Timer.measureExecutionTime(() -> InsertionSort.voidSort(random_array_1));
-        randomArrayAscendingTime1 = Timer.measureExecutionTime(() -> InsertionSort.voidSort(random_ascending_array_1));
-        randomArrayDescendingTime1 = Timer.measureExecutionTime(() -> InsertionSort.voidSort(random_descending_array_1));
-        System.out.println("Il randomArray() ordinato: " + Arrays.toString(random_array_1));
-        System.out.println("Il randomArrayAscending() ordinato: " + Arrays.toString(random_ascending_array_1));
-        System.out.println("Il randomArrayDescending() ordinato: " + Arrays.toString(random_descending_array_1));
-        System.out.println("Execution Time di Insertion sort su randomArray(): " + randomArrayTime1 + " ns.");
-        System.out.println("Execution Time di Insertion sort su randomArrayAscending(): " + randomArrayAscendingTime1 + " ns.");
-        System.out.println("Execution Time di Insertion sort su randomArrayDescending(): " + randomArrayDescendingTime1 + " ns.");
-        System.out.println("#########################################################################################");
-        System.out.println("########################## TABELLA DEI TEMPI DI BUBBLE SORT #############################");
-        System.out.println("#########################################################################################");
-        long randomArrayTime2, randomArrayAscendingTime2, randomArrayDescendingTime2;
-        randomArrayTime2 = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_array_2));
-        randomArrayAscendingTime2 = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_ascending_array_2)) ;
-        randomArrayDescendingTime2 = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_descending_array_2));
-        System.out.println("Il randomArray() ordinato: " + Arrays.toString(random_array_2));
-        System.out.println("Il randomArrayAscending() ordinato: " + Arrays.toString(random_ascending_array_2));
-        System.out.println("Il randomArrayDescending() ordinato: " + Arrays.toString(random_descending_array_2));
-        System.out.println("Execution Time di Bubble sort su randomArray(): " + randomArrayTime2 + " ns.");
-        System.out.println("Execution Time di Bubble sort su randomArrayAscending(): " + randomArrayAscendingTime2 + " ns.");
-        System.out.println("Execution Time di Bubble sort su randomArrayDescending(): " + randomArrayDescendingTime2 + " ns.");
-        System.out.println("#########################################################################################");
-        System.out.println("######################################## FINE ###########################################");
-        System.out.println("#########################################################################################");
+        //Printing.print1();
+        int[] arrLength={10,1000,100000};
+        int resultsNumber=10;
+        long[][] arrTime1= new long[resultsNumber][arrLength.length];
+        System.out.println("Bubble Sort");
+        for(int i=0;i<arrLength.length;i++){
+            creaTempiOrdinamento(resultsNumber,arrLength[i],arrTime1);
+            System.out.println("Caso di numero elementi: "+ arrLength[i]);
+            System.out.println(Arrays.deepToString(arrTime1));
+
+        }
+        System.out.println("Insertion Sort");
+        for(int i=0;i<arrLength.length;i++){
+            creaTempiOrdinamento1(resultsNumber,arrLength[i],arrTime1);
+            System.out.println("Caso di numero elementi: "+ arrLength[i]);
+            System.out.println(Arrays.deepToString(arrTime1));
+
+        }
+    }
+
+    private static void creaTempiOrdinamento1(int resultsNumber, int length, long[][] arrTime) {
+        int low=0;
+        int up= 10000000;
+        for (int i=0;i<resultsNumber;i++) {
+            int[] random_array = Generator.randomArray(length, low, up);
+            int[] random_ascending_array = Generator.randomArrayAscendingSimple(length, low, up); // Generator.randomArrayAscending(length, low, up);
+            int[] random_descending_array = Generator.randomArrayDescendingSimple(length, low, up);// Generator.randomArrayDescending(length, low, up);
+
+            long randomArrayTime, randomArrayAscendingTime, randomArrayDescendingTime;
+            randomArrayTime = Timer.measureExecutionTime(() -> InsertionSort.voidSort(random_array));
+            randomArrayAscendingTime = Timer.measureExecutionTime(() -> InsertionSort.voidSort(random_ascending_array));
+            randomArrayDescendingTime = Timer.measureExecutionTime(() -> InsertionSort.voidSort(random_descending_array));
+            arrTime[i][0]=randomArrayTime;
+            arrTime[i][1]=randomArrayAscendingTime;
+            arrTime[i][2]=randomArrayDescendingTime;
+        }
+    }
+
+    private static void creaTempiOrdinamento(int resultsNumber, int length, long[][] arrTime) {
+        int low=0;
+        int up= 10000000;
+        for (int i=0;i<resultsNumber;i++) {
+            int[] random_array = Generator.randomArray(length, low, up);
+            int[] random_ascending_array = Generator.randomArrayAscendingSimple(length, low, up); // Generator.randomArrayAscending(length, low, up);
+            int[] random_descending_array = Generator.randomArrayDescendingSimple(length, low, up);// Generator.randomArrayDescending(length, low, up);
+
+            long randomArrayTime, randomArrayAscendingTime, randomArrayDescendingTime;
+            randomArrayTime = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_array));
+            randomArrayAscendingTime = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_ascending_array)) ;
+            randomArrayDescendingTime = Timer.measureExecutionTime(() -> BubbleSort.bubbleSort(random_descending_array));
+            arrTime[i][0]=randomArrayTime;
+            arrTime[i][1]=randomArrayAscendingTime;
+            arrTime[i][2]=randomArrayDescendingTime;
+        }
     }
 }
